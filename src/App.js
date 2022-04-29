82715a005bad6c1dd944a7f46cb5549a3f25f709
Fernando-Leiva <99757333+Fernando-Leiva@users.noreply.github.com>
2022-04-29 16:18:07 -0600

    first commit. (#1)
    * Initialize project using Create React App
    
    * first commit.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Picture } from './components/picture.js/Picture';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Picture/>
+    </div>
+  );
+}
+
+export default App;
