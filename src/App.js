7c56d7b9dc3ecf741b3d770fc46472fd7fdad3d0
gildarivera <gilda.rivera@telus.com>
2022-05-01 21:34:39 -0600

    feat: Login and redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import './App.css';
-import {store} from './redux/store'
+import store from "./redux/index"
 import { Provider } from 'react-redux'
 import Sidebar from './components/sidebar/Sidebar';
+import { Login } from './components/Login/Login';
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 
   return (
+    <Provider store={store}>
     <Router>
       <div className="App">
        
           <Routes >
             {/* <Route exact path='/' element={< Login />}></Route> 
             <Route exact path='/signUp' element={< SingUp />}></Route> */}
+            <Route exact path='/' element = {<Login />}/>
+            <Route exact path='/create' element = {<></>}/>
             {/* localStorage.getItem('user') */ true ? <Route exact path='/home/*' element = {<Sidebar/>}/> : <></>}
+            
           </Routes>
       </div>   
-    </Router>
+    </Router>  </Provider>
    
   );
 }
