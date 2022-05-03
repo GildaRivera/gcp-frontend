05fa608f47dbac6d09df1c8c762de1fdcbbe2ba0
Fernando-Leiva <99757333+Fernando-Leiva@users.noreply.github.com>
2022-05-03 12:35:32 -0600

    Url (#10)
    * feat: change url
    
    * feat: URL-GKE

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const URL = 'http://localhost:3001/api'
+const URL = process.env.REACT_APP_ENDPOINT_API;
 
 export const uploadPicture = async (formData) => {
     try {
